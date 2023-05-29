cf849bdf64615538c7f1f96ffa0eeabc3e8dfff0
williams-sc <134900768+williams-sc@users.noreply.github.com>
2023-05-29 00:14:47 -0300

    Delete api directory

--- api/src/B3.CDB.API/Service/ICdbCalculatorService.cs
+++
@@ -1,9 +0,0 @@
-using B3.CDB.API.ViewModel;
-
-namespace B3.CDB.API.Service
-{
-    public interface ICdbCalculatorService
-    {
-        CdbCalculatorResponse CdbCalculate(CdbCalculatorRequest request);
-    }
-}
